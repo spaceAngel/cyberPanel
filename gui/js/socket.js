d046038596619281b8f1dcbc6cd2f3c7e8734c1e
spaceAngel <spaceangel@cyberdeck.cz>
2019-11-16 23:01:42 +0100

    [gui] callback handlers for commands

--- gui/js/socket.js
+++ gui/js/socket.js
@@ -2,6 +2,8 @@ socket = {
 
 	conn: null,
 
+	handlers: {},
+
 	open: function() {
 		this.conn = new WebSocket('ws://192.168.0.2:8080');
 		this.conn.onmessage = this.onMessage;
@@ -21,7 +23,14 @@ socket = {
 	},
 
 	onMessage: function(data) {
-		console.log(JSON.parse(data.data));
+		var data = (JSON.parse(data.data));
+		if (socket.handlers[data.command] !== undefined) {
+			socket.handlers[data.command](data.response);
+		}
+	},
+
+	registerHandler: function(command, handler) {
+		this.handlers[command] = handler;
 	}
 
 }
