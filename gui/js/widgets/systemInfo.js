3e613d8a43797b3991e80305380855b46768b425
spaceAngel <spaceangel@cyberdeck.cz>
2019-11-17 22:36:18 +0100

    [gui] system info storages data widget

--- gui/js/widgets/systemInfo.js
+++ gui/js/widgets/systemInfo.js
@@ -1,6 +1,19 @@
 var systemInfoWidget = {
 	handle: function(data) {
-		console.log(data.temperatures);
 		templates.display('.js-temperature', data.temperatures);
+
+		$('.js-storages').html('');
+		for (var i = 0; i<data.storages.length; i++) {
+			var template = $('.js-template-storage').clone();
+			$(template).removeClass('js-template-storage').removeClass('template');
+			templates.display(template, data.storages[i]);
+			$(template).find('.js-bar').css(
+				"width",
+				data.storages[i].used  * 100 / data.storages[i].size + '%' 
+			);
+
+			$('.js-storages').append(template);
+		}
+		
 	}
 }
