998feab3941b539babff6e3f9d6fd1bf7b2cec1b
spaceAngel <spaceangel@cyberdeck.cz>
2019-11-17 22:50:57 +0100

    [gui] humanize file sizes feature

--- gui/js/widgets/systemInfo.js
+++ gui/js/widgets/systemInfo.js
@@ -14,6 +14,28 @@ var systemInfoWidget = {
 
 			$('.js-storages').append(template);
 		}
+		systemInfoWidget.humanizeBytes();
 		
-	}
+	},
+
+
+	humanizeBytes: function() {
+		$('.js-bytes').each(function(index, elm) {
+			size = $(elm).html();
+			$(elm).html(systemInfoWidget.bytesToHuman(size))
+		});
+	},
+
+	bytesToHuman: function(size) {
+		var i = 0;
+		var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+		do {
+			size = size / 1024;
+			i++;
+		} while (size > 1024);
+
+		return Math.max(size, 0.1).toFixed(1) + byteUnits[i];
+	} 
+
+
 }
