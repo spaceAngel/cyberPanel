636088fd9c3adc027726e03ea4fd0914b574e638
spaceAngel <spaceangel@cyberdeck.cz>
2019-11-24 17:02:49 +0100

    [gui] returning nosleep.js

--- gui/js/environment.js
+++ gui/js/environment.js
@@ -1,7 +1,17 @@
 environment = {
 
+	enableNoSleep : function() {
+		try {
+			var noSleep = new NoSleep();
+			noSleep.enable();
+		} catch (e) {
+			console.log(456);
+		}
+	},
+	
 	enableFullScreen : function() {
 		document.documentElement.requestFullscreen();
 	}
+	
 	
 }
