d6f94121112ec7042c3e999b691ce2721e33d495
spaceAngel <spaceangel@cyberdeck.cz>
2019-12-24 01:01:03 +0100

    [daemon] media control returns currently played song and position

--- src/CyberPanel/Commands/Commands/MediaCommand.php
+++ src/CyberPanel/Commands/Commands/MediaCommand.php
@@ -12,8 +12,45 @@ class MediaCommand extends BaseCommand {
 
 		}
 
-		return [
-			'volume' => (int)Executer::execAndGetResponse(Media::CMD_VOLUME),
-		];
+		if (!empty(Executer::execAndGetResponse(Media::CMD_GETPLAYERS))) {
+			$id3 = $this->getCurrentSong();
+			return [
+				'volume' => (int)Executer::execAndGetResponse(Media::CMD_VOLUME),
+				'currentsong' => $this->getSongName($id3),
+				'length' => $id3['length'],
+				'position' => (int)Executer::execAndGetResponse(Media::CMD_CURRENTPOSITION),
+			];
+		} else {
+			return [
+				'volume' => (int)Executer::execAndGetResponse(Media::CMD_VOLUME),
+				'currentsong' => '',
+				'length' => 1,
+				'position' => 1,
+			];
+		}
+	}
+
+	private function getSongName(array $id3) : string {
+		return $id3['author'] . ' - ' . $id3['title'];
+	}
+
+	private function getCurrentSong() : array {
+		$id3 = [];
+		$rawData = explode("\n", Executer::execAndGetResponse(Media::CMD_CURRENTSONG));
+		foreach ($rawData as $dataItem) {
+
+			if (strpos($dataItem, 'xesam:artist') === 0) {
+				$id3['author'] = substr($dataItem, 14);
+			}
+
+			if (strpos($dataItem, 'xesam:title') === 0) {
+				$id3['title'] = substr($dataItem, 13);
+			}
+
+			if (strpos($dataItem, 'mpris:length') === 0) {
+				$id3['length'] = (int)substr($dataItem, 14);
+			}
+		}
+		return $id3;
 	}
 }
--- src/CyberPanel/System/ShellCommands/Media.php
+++ src/CyberPanel/System/ShellCommands/Media.php
@@ -6,6 +6,11 @@ interface Media {
 
 	// phpcs:disable Generic.Files.LineLength
 	const CMD_VOLUME = "pactl list sinks | grep '^[[:space:]]Volume:' | head -n 1 |sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,'";
+
+	const CMD_GETPLAYERS = "qdbus | egrep -i 'org.mpris.MediaPlayer'";
+
+	const CMD_CURRENTSONG = "qdbus `qdbus | egrep -i 'org.mpris.MediaPlayer'`  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata";
+	const CMD_CURRENTPOSITION = "qdbus `qdbus | egrep -i 'org.mpris.MediaPlayer'`  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Position";
 	// phpcs:enable
 
 }
