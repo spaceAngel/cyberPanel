270b829d3232e037c9b731ec6d9b3f61cc360e5a
spaceAngel <spaceangel@cyberdeck.cz>
2020-02-15 17:06:23 +0100

    [build] use uglify for es javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
 	grunt.loadNpmTasks('grunt-bake');
 	grunt.loadNpmTasks('grunt-concat-css');
 	grunt.loadNpmTasks('grunt-contrib-less');
