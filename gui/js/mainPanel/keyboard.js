7692c4ffe3b268769baedee06f982273e2540d2b
spaceAngel <spaceangel@cyberdeck.cz>
2020-06-05 16:18:33 +0200

    [gui] numeric keyboard panel

---
+++ gui/js/mainPanel/keyboard.js
@@ -0,0 +1,9 @@
+/* global cyberPanel, mixins, environment, sound */
+mixins.push({
+	methods: {
+		keyPress: function(event) {
+			socket.send('keypress', event.target.getAttribute('data-key'));
+		}
+
+	}
+});
