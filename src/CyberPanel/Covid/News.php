d9ffd4ef07c87dba1c1ed2f6762c752c6d06aadb
spaceAngel <spaceangel@cyberdeck.cz>
2020-10-20 17:32:44 +0200

    [gui] covid pane rss type article

--- src/CyberPanel/Covid/News.php
+++ src/CyberPanel/Covid/News.php
@@ -5,16 +5,18 @@ namespace CyberPanel\Covid;
 
 use CyberPanel\Covid\NewsParsers\IdnesOnlineNews;
 use CyberPanel\Covid\NewsParsers\Parser;
+use CyberPanel\Covid\NewsParsers\RssNews;
 
 class News {
 
-	protected $idnesNews;
+	const URL_RSS_ECDC = 'https://www.ecdc.europa.eu/en/taxonomy/term/2942/feed';
 
 	protected $parsers = [];
 
 	public function __construct() {
 		$this->parsers = [
-			new IdnesOnlineNews()
+			new RssNews(self::URL_RSS_ECDC),
+			new IdnesOnlineNews(),
 		];
 	}
 
