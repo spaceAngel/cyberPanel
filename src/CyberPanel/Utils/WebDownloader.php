bae29ea1b095eddaedf98bc47cb3f72febd5f1e5
spaceAngel <spaceangel@cyberdeck.cz>
2021-01-29 01:42:39 +0100

    [daemon] web donwloader can work with post request (and params) #feature

--- src/CyberPanel/Utils/WebDownloader.php
+++ src/CyberPanel/Utils/WebDownloader.php
@@ -7,13 +7,18 @@ use CyberPanel\Exceptions\RemoteContentNotDownloadedException;
 class WebDownloader {
 
 
-	public static function download(string $url) : string {
+	public static function download(string $url, string $postParams = NULL) : string {
 		$curl = curl_init();
 
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($curl, CURLOPT_HEADER, FALSE);
 		curl_setopt($curl, CURLOPT_ENCODING, '');
+		if ($postParams != NULL) {
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_SSL_CIPHER_LIST, 'DEFAULT@SECLEVEL=1');
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $postParams);
+		}
 
 		$rslt = curl_exec($curl);
 		$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
