f5e568c06f77196c6f1bad932f172520ebdf76c2
spaceAngel <spaceangel@cyberdeck.cz>
2021-10-19 00:18:22 +0200

    [addon] added download id into stored struct #feature

--- browserAddon/firefox/downloadNotify.js
+++ browserAddon/firefox/downloadNotify.js
@@ -1,22 +1,23 @@
+/* global browser */
 var downloadNotify = {
 
 	interval: null,
 	socket: null,
-	
+
 	init: function() {
 		downloadNotify.socket = new WebSocket('ws://127.0.0.1:8081');
 		downloadNotify.socket.onmessage = function(e) {}
 		downloadNotify.socket.onclose = this.handleDisconnect;
 	},
-	
+
 	startNotifying: function() {
 		downloadNotify.interval = setInterval(function(e){
 			var sendInfo = new Array();
 			browser.downloads.search({}).then(function(search) {
-				
 				for (var download of search) {
 					if (download.state == 'in_progress') {
 						sendInfo.push({
+							id: download.id,
 							filename: download.filename,
 							bytesReceived: download.bytesReceived,
 							bytesTotal: download.totalBytes,
@@ -24,17 +25,17 @@ var downloadNotify = {
 						});
 					}
 				}
-				
+
 				downloadNotify.send(sendInfo);
-				
+
 				if (sendInfo.length == 0) {
 					downloadNotify.stopNotyfing();
 				}
 			});
 		}, 2000);
 	},
-	
-	send: async function(sendInfo) {	
+
+	send: async function(sendInfo) {
 		downloadNotify.socket.send(
 			JSON.stringify({
 				command: 'storeDownloads',
@@ -42,13 +43,13 @@ var downloadNotify = {
 			})
 		);
 	},
-	
+
 	stopNotyfing: function() {
 		if (downloadNotify.interval !== null) {
 			clearInterval(downloadNotify.interval);
 		}
 	},
-	
+
 	handleDisconnect: function() {
 		setTimeout(function() {
 			downloadNotify.init();
